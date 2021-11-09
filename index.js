4618f1b33b944f73006e12680ee533cd9f94b602
Jesus <jesus.arreola@plai.mx>
2021-11-09 13:50:24 -0600

    correct

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
 
 app.get('/hw', (req, res) => {
-  res.send('Hello World 2!')
-})
+  res.send('Hello World 2!');
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Our app is running on port ${ PORT }`);
+});
